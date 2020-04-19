b34fa62fb6ebd89b1cf24c5ef785f2aea5aa81e5
tripathiravi07 <39208797+tripathiravi07@users.noreply.github.com>
2020-04-19 20:48:43 +0530

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,1 +1,10 @@
+<?php
+$mysqli = new mysqli("mydb","admin", "13dlql*41");
 
+// Check connection
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+echo "Connected successfully";
+?>
